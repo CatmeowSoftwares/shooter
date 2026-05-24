c325750e68382425684f50d510436d16e74cad4a
MysticSlime <jorenjacob098@gmail.com>
2026-05-24 23:26:32 +0800

    fixed infinite loop

--- railgun.gd
+++ railgun.gd
@@ -9,4 +9,5 @@ func use(user: Character):
 			var collider = shape_cast.get_collider(i)
 			if collider is Enemy:
 				collider.kill()
+				shape_cast.add_exception(collider)
 		shape_cast.force_shapecast_update()
